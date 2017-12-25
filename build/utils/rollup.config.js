4e26297db04777ad8938f67503d2576bd912fca4
820514 <820514@nd>
2017-12-25 21:06:51 +0800

    优化

--- build/utils/rollup.config.js
+++ build/utils/rollup.config.js
@@ -1,57 +1,43 @@
-const commonjs = require('rollup-plugin-commonjs')
-const babel = require('rollup-plugin-babel')
-const resolve = require('rollup-plugin-node-resolve')
-const uglify = require('rollup-plugin-uglify')
 const path = require('path')
-const postcss = require('rollup-plugin-postcss')
-const json = require('rollup-plugin-json')
-const simplevars = require('postcss-simple-vars')
-const nested = require('postcss-nested')
-const cssnext = require('postcss-cssnext')
-const cssnano = require('cssnano')
-const cssModules = require('postcss-modules')
-const url = require('postcss-url')
 const pkg = require('../../package.json')
 const external = Object.keys(pkg.dependencies)
 const plugins = [,
-  json({
+  require('rollup-plugin-json')({
     include: 'src/**',
     exclude: ['node_modules']
   }),
-  postcss({
+  require('rollup-plugin-postcss')({
     plugins: [
       require('postcss-flexible')({remUnit: 75}),
-      simplevars(),
-      nested(),
-      cssnext({warnForDuplicates: false}),
-      cssnano(),
-      url({
+      require('postcss-simple-vars')(),
+      require('postcss-nested')(),
+      require('postcss-cssnext')({warnForDuplicates: false}),
+      require('cssnano')(),
+      require('postcss-url')({
         url: 'copy',
         basePath: path.resolve('src/password/styles'),
         assetsPath: path.resolve('libs')
       }),
-      cssModules({
+      require('postcss-modules')({
         generateScopedName: '[local]__[hash:base64:5]'
       })
     ],
     extensions: ['.css']
   }),
-  resolve({
+  require('rollup-plugin-node-resolve')({
     jsnext: true,
     main: true,
     browser: true
   }),
-  commonjs(),
-  babel({
+  require('rollup-plugin-commonjs')(),
+  require('rollup-plugin-babel')({
     exclude: 'node_modules/**' // 仅仅转译我们的源码
   }),
-  uglify()
+  require('rollup-plugin-uglify')()
 ]
 
 module.exports = {
-  plugins: plugins,
-  external: external,
-  globals: Object.keys(external).map((value) => {
-    return {[value]: value}
-  })
+  plugins,
+  external,
+  globals: Object.keys(external).map(value => ({[value]: value}))
 }
